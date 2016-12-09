b06aca5dcadd14cd0e6179bcfe04633ad0eaf619
lutangar <johan.dufour@gmail.com>
2016-12-09 18:30:55 +0100

    bump database build

--- src/js/common/store.js
+++ src/js/common/store.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
-import initialState from '../../../database.js';
+import initialState from '../../../database.json';
 
 export default createStore((state, action) => state, initialState);
