b321701664f3031fbeeeabdf9ac6ee271f09131a
lutangar <johan.dufour@gmail.com>
2016-12-09 18:27:02 +0100

    fix volume order

--- src/js/common/Container/HomeContainer.js
+++ src/js/common/Container/HomeContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Home from '../Component/Page/Home';
 
 function mapStateToProps(state) {
-  const volumes = state.volumes.map(volume => ({
+  const volumes = state.volumes.sort((a, b) => a.number > b.number ? 1 : -1).map(volume => ({
     ...volume,
     flowers: volume.flowers.map(id => ({ ...state.flowers.find(flower => flower.id === id) })),
   }));
